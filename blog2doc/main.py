e11bb5d5617c438843956b9ef19ada62595401ac
Valeriy Osipov <info@vosipov.com>
2017-02-22 10:48:33 +0300

    Paging implemented

--- blog2doc/main.py
+++ blog2doc/main.py
@@ -3,60 +3,60 @@
 import requests
 from lxml import html
 from pathlib import Path
-import docx
-import win32com.client
 
 DATA_URL = 'http://exampleofablog.com/'
 DATA_DIR = Path(Path.cwd().parent, 'data')
 
 
 class CMain(object):
-    url = ''
+    url = None
+    cur_page_tree = None
+    page_num = 0
+    out_tree = None
+    content_node = None
 
     def __init__(self, url):
         self.url = url
 
-    def run(self):
-        page = requests.get(self.url)
-        tree = html.fromstring(page.content)
-        out_tree = None
-        content_node = None
-        for i, link in enumerate(tree.xpath(".//article/header/hgroup/h2/a/@href")):
+    def next(self):
+        next_link = (self.cur_page_tree.xpath('.//*[contains(concat(" ", normalize-space(@class), " "), '
+                                              '" pagination_container ")]/nav/*[contains(concat(" ", normalize-space('
+                                              '@class), " "), " current ")]/following-sibling::a[1]/@href') or [
+                         None])[0] if self.cur_page_tree is not None else self.url
+        if next_link is not None:
+            page = requests.get(next_link)
+            self.cur_page_tree = html.fromstring(page.content)
+            self.page_num += 1
+            return True
+        else:
+            return False
+
+    def process_page(self):
+        for i, link in enumerate(self.cur_page_tree.xpath(".//article/header/hgroup/h2/a/@href")):
             post = requests.get(link)
             post_tree = html.fromstring(post.content)
-
-            if 0 == i:
+            if self.out_tree is None:
                 for node in post_tree.xpath(
                         '//*[not(descendant::*[contains(concat(" ", normalize-space(@class), " "), " post ")]) and not('
                         'ancestor-or-self::*[contains(concat(" ", normalize-space(@class), " "), " post ")]) and '
                         'ancestor::body]'):
                     node.getparent().remove(node)
-                out_tree = post_tree
-                content_node = out_tree.xpath(".//*[@id='content']")[0]
+                self.out_tree = post_tree
+                self.content_node = self.out_tree.xpath(".//*[@id='content']")[0]
             else:
                 node = post_tree.xpath('.//*[contains(concat(" ", normalize-space(@class), " "), " post ")]')[0]
-                content_node.append(node)
+                self.content_node.append(node)
 
-        page_file_name = Path(DATA_DIR, f'page.html')
-        with open(page_file_name, 'wb') as file:
-            file.write(html.tostring(out_tree, encoding='unicode').encode('utf-8'))
+    def save_blog(self):
+        file_name = Path(DATA_DIR, f'blog.html')
+        with open(file_name, 'wb') as file:
+            file.write(html.tostring(self.out_tree, encoding='unicode').encode('utf-8'))
 
-
-
-
-                # posts = tree.xpath('.//*[contains(concat(" ", normalize-space(@class), " "), " post ")]')
-                # for i, post in enumerate(posts):
-                #     with open(Path(DATA_DIR, f'post_{i}.html'), 'wb') as file:
-                #         file.write(html.tostring(post, encoding='unicode').encode('utf-8'))
-
-                # word = win32com.client.Dispatch('Word.Application')
-                # doc = word.Documents.Add(page_file_name)
-                # # doc.SaveAs(Path(DATA_DIR, 'page.docx'), FileFormat=0)
-                # doc.Close()
-                # word.Quit()
-
+    def run(self):
+        while self.next():
+            self.process_page()
+        self.save_blog()
 
 CMain(DATA_URL).run()
 
-# TODO: Видео некорректно вставляется
-# TODO: переписать это все на Scrapy для асинхронности
+# TODO: переписать это все на Scrapy для асинхронности. Сравнить скорость загрузки всего блога в Scrapy и без Scrapy
